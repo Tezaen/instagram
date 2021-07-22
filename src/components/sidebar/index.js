fe01f02f5640299a86962cb12094084c0c2d412e
Tezaen <Philippines420AD>
2021-07-21 22:13:10 -0700

    Doing sidebar, did user part of sidebar, doing suggested users

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,22 @@
+import React, { memo } from 'react';
+import useUser from '../../hooks/use-user';
+import User from './user';
+import Suggestions from './suggestions';
+
+const Sidebar = () => {
+    const { 
+        user: { fullName, username, userId, following } 
+    } = useUser();
+    // console.log('fullName userName userId', fullName, username, userId);
+
+    return (
+        <div className="p-4">
+            <User username={username} fullName={fullName} />
+            <Suggestions userId={userId} following={following}/>
+        </div>
+    )
+}
+
+export default Sidebar;
+
+// Sidebar.whyDidYouRender = true;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import Header from '../components/header';
-import Sidebar from '../components/sidebar';
+import Sidebar from '../components/sidebar/index';
 import Timeline from '../components/timeline';
 
 const Dashboard = () => {
@@ -10,7 +10,7 @@ const Dashboard = () => {
     }, []);
 
     return (
-        <div classBane="bg-gray-background">
+        <div className="bg-gray-background">
             <Header />
             <div className="grid grid-cols-3 gap-4 justify-between 
                 mx-auto max-w-screen-lg">
